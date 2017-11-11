b52984e7f3523553a8c372322a3dfeba8f09affa
Tanmay Bansal <tanmaybansal>
2017-11-11 17:44:21 -0500

    Get Involved - Updates

--- getinvolved.php
+++ getinvolved.php
@@ -1,97 +1,46 @@
 
-<?php $header_id = "intro"; $header_title = "Get Involved"; $header_description = "Local food and artisanal goods brought to Cornell University with perfection since 2010. Every Thursday on Arts Quad from 11-2!"; $page_title="Home Page"; include 'include/header.php';?>
-      <section id="overview">
-        <div class="sm-container">
-            <div class="row">
-              <div class="col-3">
-                <div class="icon-sustainable">
-                  <img src="img/icons/team.png" alt="group of people icon">
-                  <!-- Icon by Freepik from flaticon.com-->
-                  <div class="icon-text">Supports healthy communities</div>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="icon-sustainable">
-                  <img src="img/icons/sprout.png" alt="sprout plant out of hand">
-                  <!-- Icon by Freepik from flaticon.com-->
-                  <div class="icon-text">Promotes Sustainability</div>
-                </div>
-              </div>
-              <div class="col-3">
-                <div class="icon-sustainable">
-                  <img src="img/icons/tractor.png" alt="tractor icons">
-                  <!-- Icon by Freepik from flaticon.com-->
-                  <div class="icon-text">Increases access to nutritious food</div>
-                </div>
-              </div>
-              <div class="clearfix"></div>
-                <p>Our mission is to organize and plan a weekly Farmers' market on Cornell's campus that allows for students, faculty, staff and the local community to come together to purchase and enjoy local goods and products. The Farmers' Market at Cornell is working to bridge the Ithaca community and Cornell, while building a greater appreciation for food that is fresh and locally sourced.</p>
-            </div>
-        </div>
-      </section>
+<?php $header_id = "getinvolved"; $header_title = "Get Involved"; $header_description = "You can apply to be on our E-board, become a vendor, or simply join our list serve!"; $page_title="Get Involved"; include 'include/header.php';?>
+
       <section id="where" >
         <div class="row">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2943.978663424147!2d-76.48666128439439!3d42.449470079181125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d0818a0f7b1b8b%3A0x92e21c1556e03f0d!2sArts+Quad%2C+Ithaca%2C+NY+14850!5e0!3m2!1sen!2sus!4v1510428178136" id="g_map" allowfullscreen></iframe>
           <div class="where-when">
             <div>
-              <h3>Where and When?</h3>
-              <p>We are located on west side of the Ag Quad and are on the Arts Quad every Thursday from 11-2 on the Cornell campus.</p>
+              <h3>We're Hiring!</h3>
+              <p>Join the FM@C Eboard and become part of a critical effort to connect our campus community with Ithaca area producers.
+              <br><br>
+              Applications are due Tuesday, May 16th for the following positions (detailed position descriptions on the application)</p>
               <!-- <button type="button" id="get-directions" class="black-button">Get directions</button> -->
             </div>
           </div>
           <div class="clearfix"></div>
         </div>
       </section>
-      <section id="gallery">
-        <div class="container">
-              <ul class="gallery-set">
-                <li>
-                  <div class="gallery-img img-1"></div>
-                </li>
-                <li>
-                  <div class="gallery-img img-2"></div>
-                </li>
-                <li>
-                  <div class="gallery-img img-3"></div>
-                </li>
-                <li>
-                  <div class="gallery-img img-4"></div>
-                </li>
-                <li>
-                  <div class="gallery-img img-5"></div>
-                </li>
-                <li>
-                  <div class="gallery-img img-6"></div>
-                </li>
-              </ul>
-          <div class="clearfix"></div>
-        </div>
-      </section>
-      <section id="options">
+
+      <section id="options_getin">
         <div class="container">
           <div class="option-container">
           <div class="row">
 
               <div class="col-3">
                 <div class="option-set">
-                  <h6>We are hiring!</h6>
-                  <p><a href="#"><button type="submit" class="submit-btn" class="black-button">Get Involved</button></a></p>
+                  <h6>Vendor Coordinator</h6>
+                  <p><a href="https://cornell.qualtrics.com/jfe/form/SV_2hQHl6ttKOvpFFr"><button type="submit" class="submit-btn" class="black-button">Apply!</button></a></p>
                 </div>
               </div>
 
 
               <div class="col-3">
                 <div class="option-set">
-                  <h6>View vendors</h6>
-                  <p><a href="#"><button type="submit" class="submit-btn" class="black-button">View Vendors</button></a></p>
+                  <h6>Public Relations Chair</h6>
+                  <p><a href="https://cornell.qualtrics.com/jfe/form/SV_3EL5U8SFYWughvL"><button type="submit" class="submit-btn" class="black-button">Apply!</button></a></p>
                 </div>
               </div>
 
             <div class="col-3">
               <div class="option-set">
-                <h6>Join our email list</h6>
-                <input type="email" name="email" id="join-email" placeholder="Enter your email here">
-                <a href="#"><button type="submit" class="submit-btn">Join</button></a>
+                <h6>Graphic Design and Merchandise Chair</h6>
+                <a href="https://cornell.qualtrics.com/jfe/form/SV_egiXJXNylVpdKrb"><button type="submit" class="submit-btn">Apply!</button></a>
               </div>
             </div>
           </div>
