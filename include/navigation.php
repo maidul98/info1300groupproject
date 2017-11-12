25bbc1a95306329dcbd79a4d047d5873f719f00e
Maidul Islam <maidul98@gmail.com>
2017-11-12 14:55:42 -0500

    Revert "Revert "Get Involved - Page""
    This reverts commit 54bc5f3dba2be55998b7ac1f6183887add35f3d7.

--- include/navigation.php
+++ include/navigation.php
@@ -5,17 +5,10 @@
     </div>
     <nav>
       <ul>
-<<<<<<< HEAD
         <li><a href="index.php">Home</a></li>
         <li><a href="aboutus.php">About Us</a></li>
         <li><a href="vendors.php">Vendors</a></li>
         <li><a href="getinvolved.php">Get involved</a></li>
-=======
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Vendors</a></li>
-        <li><a href="#">Get involved</a></li>
->>>>>>> parent of 979e9e9... Get Involved - Page
         <li><a href="#">Blog</a></li>
       </ul>
     </nav>
