7b6c1d6dbe33ad737582c3c2e655d4bdf8864ed2
Tanmay Bansal <tanmaybansal>
2017-11-12 14:04:10 -0500

    z

--- getinvolved.php
+++ getinvolved.php
@@ -1,7 +1,7 @@
 
 <?php $header_id = "getinvolved"; $header_title = "Get Involved"; $header_description = "You can apply to be on our E-board, become a vendor, or simply join our list serve!"; $page_title="Get Involved"; include 'include/header.php';?>
 
-      <section id="where" >
+      <section id="where">
         <div class="row">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2943.978663424147!2d-76.48666128439439!3d42.449470079181125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d0818a0f7b1b8b%3A0x92e21c1556e03f0d!2sArts+Quad%2C+Ithaca%2C+NY+14850!5e0!3m2!1sen!2sus!4v1510428178136" id="g_map" allowfullscreen></iframe>
           <div class="where-when">
@@ -65,6 +65,5 @@
           </div>
         </div>
       </section>
-
 
 <?php include 'include/footer.php' ?>
--- include/navigation.php
+++ include/navigation.php
@@ -8,7 +8,7 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="aboutus.php">About Us</a></li>
         <li><a href="vendors.php">Vendors</a></li>
-        <li><a href="#">Get involved</a></li>
+        <li><a href="getinvolved.php">Get involved</a></li>
         <li><a href="#">Blog</a></li>
       </ul>
     </nav>
